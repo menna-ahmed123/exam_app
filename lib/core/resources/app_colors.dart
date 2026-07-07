47ff7cd3b3c0b02eb450accc01d0b2fd1f74a12c
menna-ahmed123 <mannaahmed458@gmail.com>
2026-07-07 14:48:24 +0300

    add strings and app colors

---
+++ lib/core/resources/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  // Primary
+  static const Color primaryBlue = Color(0xFF02369C);
+
+  // Text Colors
+  static const Color primaryText = Color(0xFF0C1015);
+  static const Color grey = Color(0xFF535353);
+  static const Color hintText = Color(0xFFA6A6A6);
+
+  // Status Colors
+  static const Color error = Color(0xFFCC1010);
+
+  // Borders
+  static const Color border = Color(0x1F1D192B);
+  
+  static const Color lightBlue = Color(0xFFDFE7F7);
+}
